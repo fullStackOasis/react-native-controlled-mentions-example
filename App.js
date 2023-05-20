c5f779eb94fc4eff091754161f2b2b94ddda8e57
fullStackOasis <fullstackdev@fullstackoasis.com>
2023-05-20 12:23:20 -0400

    Copied App.js from example folder in 'react-native-controlled-mentions' package. There is one difference in this file. It has isInsertSpaceAfterMention set to true for the "@" trigger.

--- App.js
+++ App.js
@@ -1,117 +1,83 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import * as React from 'react';
+import { FC, useState } from 'react';
+import { Pressable, SafeAreaView, Text, View } from 'react-native';
+import { MentionInput, MentionSuggestionsProps, Suggestion } from 'react-native-controlled-mentions';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const users = [
+  {id: '1', name: 'David Tabaka'},
+  {id: '2', name: 'Mary'},
+  {id: '3', name: 'Tony'},
+  {id: '4', name: 'Mike'},
+  {id: '5', name: 'Grey'},
+];
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const hashtags = [
+  {id: 'todo', name: 'todo'},
+  {id: 'help', name: 'help'},
+  {id: 'loveyou', name: 'loveyou'},
+];
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const renderSuggestions: (suggestions: Suggestion[]) => FC<MentionSuggestionsProps> = (suggestions) => (
+  {keyword, onSuggestionPress},
+) => {
+  if (keyword == null) {
+    return null;
+  }
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      {suggestions
+        .filter(one => one.name.toLocaleLowerCase().includes(keyword.toLocaleLowerCase()))
+        .map(one => (
+          <Pressable
+            key={one.id}
+            onPress={() => onSuggestionPress(one)}
+
+            style={{padding: 12}}
+          >
+            <Text>{one.name}</Text>
+          </Pressable>
+        ))
+      }
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const renderMentionSuggestions = renderSuggestions(users);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const renderHashtagSuggestions = renderSuggestions(hashtags);
+
+const App = () => {
+  const [value, setValue] = useState('Hello @[Mary](2)! How are you?');
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <SafeAreaView>
+      <MentionInput
+        value={value}
+        onChange={setValue}
+
+        partTypes={[
+          {
+            trigger: '@',
+            renderSuggestions: renderMentionSuggestions,
+            isInsertSpaceAfterMention: true,
+          },
+          {
+            trigger: '#',
+            renderSuggestions: renderHashtagSuggestions,
+            textStyle: {fontWeight: 'bold', color: 'grey'},
+          },
+          {
+            pattern: /(https?:\/\/|www\.)[-a-zA-Z0-9@:%._\+~#=]{1,256}\.(xn--)?[a-z0-9-]{2,20}\b([-a-zA-Z0-9@:%_\+\[\],.~#?&\/=]*[-a-zA-Z0-9@:%_\+\]~#?&\/=])*/gi,
+            textStyle: {color: 'blue'},
+          },
+        ]}
+
+        placeholder="Type here..."
+        style={{padding: 12}}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
